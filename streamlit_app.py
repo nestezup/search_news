3779c8165dc293782f4f126dcd024db67b222dd0
minilab <a@a.com>
2024-08-18 19:27:10 +0900

    app수정

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from docx import Document
 from io import BytesIO
 import time
@@ -18,9 +15,9 @@ def setup_driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
-    # Use default ChromeDriver for Streamlit Cloud
-    driver = webdriver.Chrome(service=Service(), options=chrome_options)
+    
+    # Default ChromeDriver configuration for Streamlit Cloud
+    driver = webdriver.Chrome(options=chrome_options)
     return driver
 
 @st.cache_resource
